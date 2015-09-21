99127703873a27d871db74fb1db81cc3b7d6e5fb
placiano <placiano80@gmail.com>
2015-09-21 23:28:15 +0200

    tools_emotion: update installer.sh
    Signed-off-by: placiano <placiano80@gmail.com>
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- tools_emotion/tmp/installer.sh
+++ tools_emotion/tmp/installer.sh
@@ -1,9 +1,9 @@
 #!/sbin/sh
-if [ ! -f /data/app/com.af.synapse-*/ ]
+if [ ! -d /data/app/com.af.synapse-1 ]
 then
-mkdir /data/app/com.af.synapse-1/
+	mkdir -p /data/app/com.af.synapse-1
     cp /tmp/base.apk /data/app/com.af.synapse-1/base.apk
-    chmod 644 /data/app/com.af.synapse-1/base.apk
+    chmod 644 /data/app/com.af.synapse-1/base.apk -R
 fi
 rm -f /tmp/base.apk
 
